c62b87c51939ed044506953f7322d028c66b26e8
Todd Smith <mail@toddbsmith.com>
2018-06-02 01:35:32 -0400

    fix crash

--- app/src/main/java/com/tbse/arvectorfields/ResolveDialogFragment.kt
+++ app/src/main/java/com/tbse/arvectorfields/ResolveDialogFragment.kt
@@ -21,6 +21,7 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentActivity
+import android.widget.EditText
 import com.google.common.base.Preconditions
 import kotlinx.android.synthetic.main.resolve_dialog.*
 
@@ -28,6 +29,7 @@ import kotlinx.android.synthetic.main.resolve_dialog.*
 class ResolveDialogFragment : DialogFragment() {
 
     private var okListener: OkListener? = null
+    private val room_code_input: EditText? = activity?.findViewById(R.id.room_code_input)
 
     fun setOkListener(okListener: OkListener) {
         this.okListener = okListener
@@ -45,7 +47,7 @@ class ResolveDialogFragment : DialogFragment() {
                 .setPositiveButton(
                         R.string.resolve_dialog_ok
                 ) { dialog, which ->
-                    val roomCodeText = room_code_input.text
+                    val roomCodeText = room_code_input?.text
                     if (okListener != null && roomCodeText != null && roomCodeText.isNotEmpty()) {
                         val longVal = java.lang.Long.valueOf(roomCodeText.toString())
                         okListener!!.onOkPressed(longVal)
@@ -54,4 +56,5 @@ class ResolveDialogFragment : DialogFragment() {
                 .setNegativeButton(R.string.cancel) { dialog, which -> }
         return builder.create()
     }
+
 }
